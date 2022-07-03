dcae2a31e145fafedc9201d08d512bbe72316135
rock9350 <101309789+rock9350@users.noreply.github.com>
2022-07-04 00:05:04 +0530

    Add files via upload

---
+++ coffe/firstclass.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class firstclass {
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+
+    String a = sc.nextLine();
+    String b = sc.nextLine();
+    System.out.printf(a);
+    System.out.println(b);
+    sc.close();
+  }
+
+}
